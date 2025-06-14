4d5bd963c82dbbb9ad619c93fdf494c529c20678
Maverick-1005 <ansh1005mishra@gmail.com>
2025-06-14 21:54:03 +0530

    imports 8

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -27,7 +27,6 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import React, { useState } from "react";
 // import { useRouter } from "next/router";
-import { useModal } from "@/hooks/use-modal-store";
 
 
 
